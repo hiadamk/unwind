479e8bd31e0a5692ed98a14127f06df771771687
Adam Kona <adam.kona4@gmail.com>
2020-07-27 10:46:10 +0100

    Waiting for images to load

--- src/utils/pupeteer_utils.js
+++ src/utils/pupeteer_utils.js
@@ -19,31 +19,6 @@ async function getScreenshot(html){
     await page.setViewport({ width: 600, height: 100, deviceScaleFactor: 2 });
 
     await page.goto(`data:text/html,${html}`, { waitUntil: 'load' });
-
-
-    // Get the height of the rendered page
-    const bodyHandle = await page.$('body');
-    const { height } = await bodyHandle.boundingBox();
-    await bodyHandle.dispose();
-
-    // Scroll one viewport at a time, pausing to let content load
-    const viewportHeight = page.viewport().height;
-    let viewportIncr = 0;
-    while (viewportIncr + viewportHeight < height) {
-        await page.evaluate(_viewportHeight => {
-        window.scrollBy(0, _viewportHeight);
-        }, viewportHeight);
-        await wait(200);
-        viewportIncr = viewportIncr + viewportHeight;
-    }
-
-    // Scroll back to top
-    await page.evaluate(_ => {
-        window.scrollTo(0, 0);
-    });
-
-    // Some extra delay to let images load
-    await wait(500);
     
     const image = await page.screenshot({fullPage: true, encoding: 'binary'});
     await browser.close();
