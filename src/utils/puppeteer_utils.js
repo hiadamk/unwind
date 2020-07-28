c85eb332c79c71568e51847ee9adca87cafb52ca
Adam Kona <adam.kona4@gmail.com>
2020-07-28 09:19:04 +0100

    Image Compression

--- src/utils/puppeteer_utils.js
+++ src/utils/puppeteer_utils.js
@@ -1,4 +1,6 @@
 const puppeteer = require('puppeteer');
+const imagemin = require('imagemin');
+const imageminPngquant = require('imagemin-pngquant');
 
 module.exports = {
     getScreenshot: getScreenshot,
@@ -18,6 +20,14 @@ async function getScreenshot(html) {
     await page.setViewport({ width: 600, height: 100, deviceScaleFactor: 2 });
     await page.setContent(html, {waitUntil: 'networkidle0'});
     const image = await page.screenshot({ fullPage: true, encoding: 'binary' });
+    const compressed = await imagemin.buffer(image,{
+		plugins: [
+			imageminPngquant({
+                quality: [0.7, 0.9],
+                speed: 1
+            })
+		]
+	})
     await browser.close();
-    return image;
+    return compressed;
 }
