76320485192e0dc36f9609b9b6f9071806ba8de7
Adam Kona <adam.kona4@gmail.com>
2020-07-28 17:42:03 +0100

    Lowering resolution of images

--- src/utils/puppeteer_utils.js
+++ src/utils/puppeteer_utils.js
@@ -12,18 +12,21 @@ async function getScreenshot(html) {
         'args': process.env.PUPPETEER_LAUNCH_ARGS.split(' ')
     });
     const page = await browser.newPage();
-    await page.setViewport({ width: 600, height: 100, deviceScaleFactor: 2 });
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36')
+    await page.setViewport({ width: 600, height: 100, deviceScaleFactor: 1 });
+    await page.goto("about:blank")
     await page.setContent(html, {waitUntil: 'networkidle0'});
     const image = await page.screenshot({ fullPage: true, encoding: 'binary' });
     const compressed = await imagemin.buffer(image,{
 		plugins: [
 			imageminPngquant({
-                quality: [0.6, 0.9],
+                quality: [0.3, 0.5],
                 speed: 8,
-                strip : true
+                strip : true,
+                dithering : 0.5
             })
 		]
-	}).catch((err) => console.log(err))
+    }).catch((err) => console.log(err))
     await browser.close();
     return compressed;
 }
