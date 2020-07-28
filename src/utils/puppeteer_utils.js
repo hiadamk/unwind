8369539149f26f9dea3913acd6b64070d5209452
Adam Kona <adam.kona4@gmail.com>
2020-07-28 09:58:13 +0100

    Puppeteer args are env variables

--- src/utils/puppeteer_utils.js
+++ src/utils/puppeteer_utils.js
@@ -6,15 +6,9 @@ module.exports = {
     getScreenshot: getScreenshot,
 };
 
-
 async function getScreenshot(html) {
     const browser = await puppeteer.launch({
-        'args': [
-            '--no-sandbox',
-            '--disable-setuid-sandbox',
-            '--disable-dev-shm-usage',
-            '--single-process',
-        ]
+        'args': process.env.PUPPETEER_LAUNCH_ARGS.split(' ')
     });
     const page = await browser.newPage();
     await page.setViewport({ width: 600, height: 100, deviceScaleFactor: 2 });
@@ -23,11 +17,12 @@ async function getScreenshot(html) {
     const compressed = await imagemin.buffer(image,{
 		plugins: [
 			imageminPngquant({
-                quality: [0.7, 0.9],
-                speed: 1
+                quality: [0.6, 0.9],
+                speed: 8,
+                strip : true
             })
 		]
-	})
+	}).catch((err) => console.log(err))
     await browser.close();
     return compressed;
 }
